15c926c61c36379ec57168fbfe60720a703fa6ec
zellgatos <zellbrawl@yahoo.com>
2017-09-25 14:43:59 -0500

    completed object oriented programming part 2 while editing functionality of method setName

--- src/Person.java
+++ src/Person.java
@@ -2,11 +2,11 @@ public class Person {
     private String name;
 
     public String getName(){
-        return ("");
+        return name;
     }
 
-    public void setName(String name){
-        Person.this.name = "dan";
+    public void setName(String newName){
+        name = newName;
     }
 
     public void sayHello(){
@@ -18,10 +18,23 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person = new Person("john");
-        person.getName();
-        person.setName("Dan");
-        person.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person1.sayHello();
     }
 }
 
